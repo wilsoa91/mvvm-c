7959987df1c7474dc0b98e4c74a41c9d3c8866d3
Andrew Wilson-Jones <andrew.wilson-jones@bbc.co.uk>
2019-12-04 11:10:58 +0000

    Initial Commit

---
+++ MVVMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Andrew Wilson-Jones on 04/12/2019.
+//  Copyright © 2019 Andrew Wilson-Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
